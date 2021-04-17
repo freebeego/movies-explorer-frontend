645cf08c44ad52c0330accbe657f6f46c14674d3
freebeego <freebeego@yandex.ru>
2021-04-18 02:49:11 +0300

    fix(main): techs

--- src/components/Main/Techs/Techs.js
+++ src/components/Main/Techs/Techs.js
@@ -13,15 +13,15 @@ function Techs() {
         На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.
       </p>
       <div className="techs__grid-container-wrapper">
-        <div className="techs__grid-container">
-          <span className="techs__grid-container-item">HTML</span>
-          <span className="techs__grid-container-item">CSS</span>
-          <span className="techs__grid-container-item">JS</span>
-          <span className="techs__grid-container-item">React</span>
-          <span className="techs__grid-container-item">Git</span>
-          <span className="techs__grid-container-item">Express.js</span>
-          <span className="techs__grid-container-item">mongoDB</span>
-        </div>
+        <ul className="techs__grid-container">
+          <li className="techs__grid-container-item">HTML</li>
+          <li className="techs__grid-container-item">CSS</li>
+          <li className="techs__grid-container-item">JS</li>
+          <li className="techs__grid-container-item">React</li>
+          <li className="techs__grid-container-item">Git</li>
+          <li className="techs__grid-container-item">Express.js</li>
+          <li className="techs__grid-container-item">mongoDB</li>
+        </ul>
       </div>
     </section>
   );
